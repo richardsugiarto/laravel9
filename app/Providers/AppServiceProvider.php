efd1fe236b294a6efb9130544da98808625e1512
richardsugiarto <96427289+richardsugiarto@users.noreply.github.com>
2022-05-03 15:22:29 +0800

    Update AppServiceProvider.php
    Solving merge

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,11 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-<<<<<<< HEAD
 	Schema::defaultStringLength(191);
-=======
         session()->put('locale', 'id');
         app()->setLocale(session()->get('locale'));
->>>>>>> 691fc2279b510085d53d33989e2c4aa948a3c28c
     }
 }
